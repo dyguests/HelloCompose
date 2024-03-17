947ca4dd69f49b425025ba5a83b0994b25331d4d
fanhl <dyguests@qq.com>
2024-03-17 18:45:30 +0800

    SimpleWidgetColumn

--- app/src/main/java/com/fanhl/hellocompose/MainActivity.kt
+++ app/src/main/java/com/fanhl/hellocompose/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.fanhl.hellocompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.fanhl.hellocompose.ui.theme.HelloComposeTheme
 
@@ -19,18 +23,38 @@ class MainActivity : ComponentActivity() {
             HelloComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    SimpleWidgetColumn()
                 }
             }
         }
+    }
+
+}
+
+@Composable
+private fun SimpleWidgetColumn() {
+    Column {
+        Text(text = "Text1")
+        Text(text = "Text1")
+        val context = LocalContext.current
+        Button(onClick = { Toast.makeText(context, "This is Toast", Toast.LENGTH_SHORT).show() }) {
+            Text(text = "Button")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SimpleWidgetColumnPreview() {
+    HelloComposeTheme {
+        SimpleWidgetColumn()
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!", modifier = modifier
     )
 }
 
