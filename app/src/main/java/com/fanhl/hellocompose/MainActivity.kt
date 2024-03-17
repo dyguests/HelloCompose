d0227e8d94735a8b72a56a53def3e1194f92c0ad
fanhl <dyguests@qq.com>
2024-03-17 19:00:52 +0800

    Image

--- app/src/main/java/com/fanhl/hellocompose/MainActivity.kt
+++ app/src/main/java/com/fanhl/hellocompose/MainActivity.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.fanhl.hellocompose.ui.theme.HelloComposeTheme
 
@@ -40,6 +44,8 @@ private fun SimpleWidgetColumn() {
         Button(onClick = { Toast.makeText(context, "This is Toast", Toast.LENGTH_SHORT).show() }) {
             Text(text = "Button")
         }
+        TextField(value = "", onValueChange = {}, placeholder = { Text(text = "Placeholder") }, colors = TextFieldDefaults.colors())
+        Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "A dog")
     }
 }
 
