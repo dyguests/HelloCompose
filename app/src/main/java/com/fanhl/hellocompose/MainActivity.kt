6cfb450769afff80f160c1697499471d965bc743
fanhl <dyguests@qq.com>
2024-03-17 19:44:57 +0800

    align

--- app/src/main/java/com/fanhl/hellocompose/MainActivity.kt
+++ app/src/main/java/com/fanhl/hellocompose/MainActivity.kt
@@ -8,12 +8,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -37,7 +39,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun SimpleWidgetColumn() {
-    Column {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(text = "Text1")
         Text(text = "Text1")
         val context = LocalContext.current
@@ -46,6 +50,7 @@ private fun SimpleWidgetColumn() {
         }
         TextField(value = "", onValueChange = {}, placeholder = { Text(text = "Placeholder") }, colors = TextFieldDefaults.colors())
         Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "A dog")
+        CircularProgressIndicator()
     }
 }
 
